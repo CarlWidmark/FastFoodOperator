f02d7141f93f0ed6c339f3bfbf267f5f11d413ca
CarlWidmark <Carl.widmark@iths.se>
2025-04-15 09:30:36 +0200

    merge to main

---
+++ Shared/DrinkDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared
+{
+    public class DrinkDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Size { get; set; }
+        public string? Unit { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
